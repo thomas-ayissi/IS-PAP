d2f47f70059c6dc9b9e1f45a2c0c7db36f0b1e90
thomas-ayissi <thomas8sea@gmail.com>
2022-05-09 18:20:02 +0200

    SaveLun9Mai2022

---
+++ exemplesPAP/src/tp00/Tri.java
@@ -0,0 +1,31 @@
+package tp00;
+
+public class Tri {
+
+    int recherche(V, nbr)
+    {
+     if
+        V.lenght =0  V=-1;
+
+     for () {
+         int a = 0;
+         int b = V.length;
+         int m = b / 2;
+     }
+
+     while (a<b Vm!=nbr)
+        if (nbr<Vm) {
+            b = m-1;
+        else {
+            a=m+1;
+            }
+        m=[(a+b)/2]
+        }
+
+     if (Vm=nbr){
+        return m
+
+        else
+            return -1
+
+     }
